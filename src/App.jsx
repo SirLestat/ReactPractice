bf2418d0cb4cae8cb9c579e58b8a5fa0f5d6807f
Edgar Alejandro Santiago Perez <alejandrosantiago.cfeteit@gmail.com>
2024-09-17 19:55:43 -0600

    Nuevos ejercicios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 export default function App() {
+  const [pelicula, setPelicula] = useState("");
+  const [ListaPeliculas, setListaPeliculas] = useState([]);
+
+  const handleChange = (e) => {
+    setPelicula(e.target.value);
+  };
+
+  const agregarPelicula = () => {
+    setListaPeliculas((prevPeliculas) => [...prevPeliculas, pelicula]);
+    setPelicula('')
+  };
+
   return (
     <div>
-      <p>Hola</p>
+      <p>Lista de películas</p>
+      <input
+        type="text"
+        name="name"
+        value={pelicula}
+        onChange={handleChange}
+        placeholder="Película"
+      />
+      <button onClick={agregarPelicula}>Agregar Película</button>
+
+      <ul style={{ listStyle: "none" }}>
+        {ListaPeliculas.map((pelicula) => {
+          return <li key={pelicula}>{pelicula}</li>;
+        })}
+      </ul>
     </div>
   );
 }
